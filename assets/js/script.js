aceff5cbce7865f3fa04c3beb7ea0b99db8e2d7f
Kingluddite <howley.phil@gmail.com>
2019-11-17 19:56:31 -0800

    Add next question button and question js text dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
   $("#name-btn").click(function() {
     $("#name").html("John Jacob Jingleheimer-Schmidt");
   });
+  $("#next-question").click(function() {
+    $("#name").html(
+      "What if the button was dynamically added to the page? How would we alter our code?"
+    );
+  });
 });
