e01c82d6b6c7ed5ef3e0a2b182d381aea1f12f4a
Kingluddite <howley.phil@gmail.com>
2019-11-17 18:33:03 -0800

    Add jquery ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,4 @@
-console.log("it works!");
-$("#root").html("<h2>I am now using jQuery to dynamically render markdown");
+$(document).ready(function() {
+  console.log("it works!");
+  $("#root").html("<h2>I am now using jQuery to dynamically render markdown");
+});
