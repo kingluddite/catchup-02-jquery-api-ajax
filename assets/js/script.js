c430a4ddfd0d624c2a521793029d5e1c61580c11
Kingluddite <howley.phil@gmail.com>
2019-11-17 19:11:16 -0800

    add clock and review questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.text(minutes + ":" + seconds);
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
 $(document).ready(function() {
   console.log("it works!");
-  $(".root").html(
-    "<h2>I am now using jQuery to dynamically render markdown</h2><p>And I'm using the Bootstrap library!"
-  );
+  var fiveMinutes = 60 * 5,
+    display = $(".card-title");
+  startTimer(fiveMinutes, display);
 });
