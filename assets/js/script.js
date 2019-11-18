17e8d5c0a8910d1379f9ef092a024a2795f74e08
Kingluddite <howley.phil@gmail.com>
2019-11-17 20:13:23 -0800

    Add correct activity 2 answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   console.log("click code works!");
-  $("#name-btn").click(function() {
+  var root = document.querySelector("#empty-area");
+  root.innerHTML =
+    "<button id='name-btn'>Click to see my name</button><div id='name'></div>";
+
+  $("#empty-area").on("click", "#name-btn", function() {
+    console.log("test");
     $("#name").html("John Jacob Jingleheimer-Schmidt");
-  });
-  $("#next-question").click(function() {
-    $("#name").html(
-      "What if the button was dynamically added to the page? How would we alter our code?"
-    );
   });
 });
