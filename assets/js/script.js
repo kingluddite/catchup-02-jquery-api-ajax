424cb7c667edbbaecc3fec4cf125483f2cfd764c
Kingluddite <howley.phil@gmail.com>
2019-11-18 00:46:23 -0800

    solution for star wars activity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
   console.log("click code works!");
+
+  var randNum = Math.floor(Math.random() * 60);
+
+  $.ajax({
+    url: "https://swapi.co/api/planets/" + randNum,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $("#root").append("<h2> Planet Name: " + response.name + "</h2>");
+    $("#root").append("<p> Terrain: " + response.terrain + "</p>");
+    $("#root").append("<p> Population: " + response.population + "</p>");
+  });
+  // shortcut alternate way
+  // $.get("https://swapi.co/api/planets/" + randNum).done(function(data) {
+  //   $("#root").append("<h2> Planet Name: " + data.name + "</h2>");
+  //   $("#root").append("<p> Terrain: " + data.terrain + "</p>");
+  //   $("#root").append("<p> Population: " + data.population + "</p>");
+  // });
 });
