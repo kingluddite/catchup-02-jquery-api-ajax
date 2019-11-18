6fec1f92d754b8f0f99f868cd82c136cc3063ea8
Kingluddite <howley.phil@gmail.com>
2019-11-17 19:51:33 -0800

    put countdown timer in its own file and made click work

---
+++ assets/js/countdown-timer.js
@@ -0,0 +1,25 @@
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.text(minutes + ":" + seconds);
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+$(document).ready(function() {
+  console.log("timer works!");
+  var fiveMinutes = 60 * 10,
+    display = $(".card-title");
+  startTimer(fiveMinutes, display);
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
-function startTimer(duration, display) {
-  var timer = duration,
-    minutes,
-    seconds;
-  setInterval(function() {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.text(minutes + ":" + seconds);
-
-    if (--timer < 0) {
-      timer = duration;
-    }
-  }, 1000);
-}
-
 $(document).ready(function() {
-  console.log("it works!");
-  var fiveMinutes = 60 * 10,
-    display = $(".card-title");
-  startTimer(fiveMinutes, display);
+  console.log("click code works!");
+  $("#name-btn").click(function() {
+    $("#name").html("John Jacob Jingleheimer-Schmidt");
+  });
 });
