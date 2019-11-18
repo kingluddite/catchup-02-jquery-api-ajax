349e632a96d1ec579d6dd464dd976f93c7f2b31a
Kingluddite <howley.phil@gmail.com>
2019-11-18 00:32:44 -0800

    Add ajax placeholder for live code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
   console.log("click code works!");
-  var root = document.querySelector("#empty-area");
-  // innerHTML is vanilla JavaScript (not jQuery)
-  // innerHTML is a property not a method
-  root.innerHTML(
-    "<button id='name-btn'>Click to see my name</button><div id='name'></div>"
-  );
-
-  // don't forget the # for an id $("#empty-area");
-  $("empty-area").on("click", "#name-btn", function() {
-    $("#name").html("John Jacob Jingleheimer-Schmidt");
-  });
 });
