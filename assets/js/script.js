6765a7583285ce83ea5f81733e6d10f0b7c3682d
Kingluddite <howley.phil@gmail.com>
2019-11-17 18:37:26 -0800

    Add working bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
   console.log("it works!");
-  $("#root").html("<h2>I am now using jQuery to dynamically render markdown");
+  $(".root").html(
+    "<h2>I am now using jQuery to dynamically render markdown</h2><p>And I'm using the Bootstrap library!"
+  );
 });
