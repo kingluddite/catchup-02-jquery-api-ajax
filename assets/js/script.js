6507a12cc3231520f98d1c546ba49daeb24e7cf5
Kingluddite <howley.phil@gmail.com>
2019-11-18 00:33:15 -0800

    Add solution for live code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
   console.log("click code works!");
+  $.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon-habitat/4/",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var speciesArr = response.pokemon_species;
+    for (var i = 0; i < speciesArr.length; i++) {
+      $("#root").append("<p>" + speciesArr[i].name + "</p>");
+    }
+  });
+  // shortcut alternate way
+  // $.get("https://pokeapi.co/api/v2/pokemon-habitat/4/").done(function(data) {
+  //   console.log(data);
+  //   var speciesArr = data.pokemon_species;
+  //   for (var i = 0; i < speciesArr.length; i++) {
+  //     $("#root").append("<p>" + speciesArr[i].name + "</p>");
+  //   }
+  // });
 });
